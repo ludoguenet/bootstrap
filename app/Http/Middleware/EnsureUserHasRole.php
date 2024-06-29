977c18e68f6d00db622d64eddb462a84ccb0f9c5
ludoguenet <ludovicguenet@gmx.com>
2024-06-29 15:01:18 +0200

    Initiam commit  tt   it t 🚀

---
+++ app/Http/Middleware/EnsureUserHasRole.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserHasRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string $role): Response
+    {
+        $user = $request->user();
+
+        abort_if($user === null || $user->doesntHaveRole($role), 404);
+
+        return $next($request);
+    }
+}
